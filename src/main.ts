6ffda3699eb96eac333a07a03bf9986a98a48236
Tayo tunmise <tunmisetayo16@gmail.com>
2024-10-10 11:01:07 -0400

    Installed Angular router using `ng add @angular/router` - Verified the router is included in the bootstrap configuration - Defined a list of routes for navigation in the application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { HospitalListComponent } from './app/hospital-list/hospital-list.component'; // Ensure the path is correct
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Define the routes for your application
+const routes: Routes = [
+  { path: 'hospital-list', component: HospitalListComponent },
+  { path: '', redirectTo: 'hospital-list', pathMatch: 'full' },
+
+];
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    ...appConfig.providers
+  ]
+}).catch((err) => console.error(err));
